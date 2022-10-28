190b2d0cf5eab0a7d1555eaee7761cb7dfaf2a3c
Sweet Shabarish <70278432+Sweetshabarish@users.noreply.github.com>
2022-10-28 13:40:00 +0530

    Update BookingWOrkingClassDataBase.java

--- TrainWithDatabase/src/TrainWithDatabase/BookingMethods/BookingWOrkingClassDataBase.java
+++ TrainWithDatabase/src/TrainWithDatabase/BookingMethods/BookingWOrkingClassDataBase.java
@@ -21,82 +21,89 @@ public  class BookingWOrkingClassDataBase {
     public static void BookingMethod() throws SQLException {
 
         System.out.println(TrainConstants.enterTheTrainNumber);//Took from the train constant class for entering the train number
-         TrainNumber = sscannerClassObject.next();//getting train number form the user
+        TrainNumber = sscannerClassObject.next();//getting train number form the user
+          ResultSet resultSetObjectForDateCheck = ExcetuteQureyThrowDatabase.connectExecuteQuery("Select * from traindetails;");
 
-        System.out.println(TrainConstants.displayDesign);//display the desigin from the trainconstants class
-        System.out.println();
-        TrainDetailsMain.trainDetailsExecution("Select trainname,trainnumber,startlocation,endlocation from traindetails where trainnumber='"+TrainNumber+"';");//the query will passed to TrainDetailClass where query will execute and gives output.
+          while (resultSetObjectForDateCheck.next()) {
+              if (TrainNumber.equals(resultSetObjectForDateCheck.getString(2))) {
+                  System.out.println(TrainConstants.displayDesign);//display the desigin from the trainconstants class
+                  System.out.println();
+                  TrainDetailsMain.trainDetailsExecution("Select trainname,trainnumber,startlocation,endlocation from traindetails where trainnumber='" + TrainNumber + "';");//the query will passed to TrainDetailClass where query will execute and gives output.
 
-        System.out.println();
-        System.out.println(TrainConstants.displayDesign);
-        System.out.println(TrainConstants.dateAndTimeDisplay);//display  date and time constatnt from the train constants
-        System.out.println("__________________________");
+                  System.out.println();
+                  System.out.println(TrainConstants.displayDesign);
+                  System.out.println(TrainConstants.dateAndTimeDisplay);//display  date and time constatnt from the train constants
+                  System.out.println("__________________________");
 
-        DateMain.dateEexcutionMethod( "Select date,seats from datedetails where trainnumber='"+TrainNumber+"'; ");//the Qury is passed Date Main class ,where the Query will execute,and show the date ,seats of the particular train ,thorugh customer input
-        System.out.println(TrainConstants.displayDesign);
+                  DateMain.dateEexcutionMethod("Select date,seats from datedetails where trainnumber='" + TrainNumber + "'; ");//the Qury is passed Date Main class ,where the Query will execute,and show the date ,seats of the particular train ,thorugh customer input
+                  System.out.println(TrainConstants.displayDesign);
 
-        System.out.println(TrainConstants.displayDesign);
-        t1:
-        while (true) {
-            System.out.println("1:Want to Book");
-            System.out.println("2:Need to check another train?");
+                  System.out.println(TrainConstants.displayDesign);
+                  t1:
+                  while (true) {
+                      System.out.println("1:Want to Book");
+                      System.out.println("2:Need to check another train?");
 
-            String PnrnUmberStoring = " ";//to store pnr number
-            int option = sscannerClassObject.nextInt();
-            if (option == 1) {
-
-
+                      String PnrnUmberStoring = " ";//to store pnr number
+                      int option = sscannerClassObject.nextInt();
+                      if (option == 1) {
 
 
-                System.out.println(TrainConstants.enterTheDate);
-                String gettingdatefromuser = sscannerClassObject.next();//to get date from the user
-                ResultSet somewhat = ExcetuteQureyThrowDatabase.connectExecuteQuery("Select * from datedetails;");
-                int StoredSeats=0;
-                while (somewhat.next()) {
-                    if (gettingdatefromuser.equals(somewhat.getString(3)) && TrainNumber.equals(somewhat.getString(1))) {
-                        ResultSet seats= ExcetuteQureyThrowDatabase.connectExecuteQuery("Select seats from datedetails where trainnumber='"+TrainNumber+"' and date='"+ gettingdatefromuser+"' ;");
-                        while(seats.next()){
-                            StoredSeats=seats.getInt(1);
-                        }
-                        System.out.println(TrainConstants.enterNumberofSeats);
-                        int seatsnumber = sscannerClassObject.nextInt();
-                        if (seatsnumber <= 4) {//the one user can book 4 ticket at a time,one pnr can contain 4 tickets
-                            PnrnUmberStoring = generateNumberObject.getPnrNumber();//gets a pnr number from pnrgenrating class
-                            StoredSeats = StoredSeats - seatsnumber;
-                            for (int innerindex = 0; innerindex < seatsnumber; innerindex++) {
-                                FileUserDatabase.AddlList(PnrnUmberStoring);//here the process will start.where this will ask the customer details and number of the tickets to book and store in ticket object
-                                TrainBookingDeatilsStroingModuleDatabase.methodForTrainBooking();
+                          System.out.println(TrainConstants.enterTheDate);
+                          String gettingdatefromuser = sscannerClassObject.next();//to get date from the user
+                          ResultSet resultSetObject = ExcetuteQureyThrowDatabase.connectExecuteQuery("Select * from datedetails;");
+                          int StoredSeats = 0;
+                          while (resultSetObject.next()) {
+                              if (gettingdatefromuser.equals(resultSetObject.getString(3)) && TrainNumber.equals(resultSetObject.getString(1))) {
+                                  ResultSet seats = ExcetuteQureyThrowDatabase.connectExecuteQuery("Select seats from datedetails where trainnumber='" + TrainNumber + "' and date='" + gettingdatefromuser + "' ;");
+                                  while (seats.next()) {
+                                      StoredSeats = seats.getInt(1);
+                                  }
+                                  System.out.println(TrainConstants.enterNumberofSeats);
+                                  int seatsnumber = sscannerClassObject.nextInt();
+                                  if (seatsnumber <= 4) {//the one user can book 4 ticket at a time,one pnr can contain 4 tickets
+                                      PnrnUmberStoring = generateNumberObject.getPnrNumber();//gets a pnr number from pnrgenrating class
+                                      StoredSeats = StoredSeats - seatsnumber;
+                                      for (int innerindex = 0; innerindex < seatsnumber; innerindex++) {
+                                          FileUserDatabase.AddlList(PnrnUmberStoring);//here the process will start.where this will ask the customer details and number of the tickets to book and store in ticket object
+                                          TrainBookingDeatilsStroingModuleDatabase.methodForTrainBooking();
 
-                            }
+                                      }
 
 //						   TrainObject.setPnr(PnrnUmberStoring);
-                            System.out.println(TrainConstants.yourPnrNumber+ PnrnUmberStoring);
-                            System.out.println(TrainConstants.leftSeatsAre + StoredSeats);
-                            String p = "UPDATE datedetails SET seats = "+StoredSeats+" WHERE date='"+gettingdatefromuser+"' and trainnumber='"+TrainNumber+"';";
-                            DateStoreDetailsDatabaseExcecution.dateStoreDetails(p);
-                        } else {
-                            System.out.println(TrainConstants.youCanBook$Tickets);
-                        }
+                                      System.out.println(TrainConstants.yourPnrNumber + PnrnUmberStoring);
+                                      System.out.println(TrainConstants.leftSeatsAre + StoredSeats);
+                                      String p = "UPDATE datedetails SET seats = " + StoredSeats + " WHERE date='" + gettingdatefromuser + "' and trainnumber='" + TrainNumber + "';";
+                                      DateStoreDetailsDatabaseExcecution.dateStoreDetails(p);
+                                  } else {
+                                      System.out.println(TrainConstants.youCanBook$Tickets);
+                                  }
 
-                    }
-
-
+                              }
 
 
-                }
-                break;
+                          }
+                          break;
 
 
-            }
+                      }
 
 
-            if (option == 2){
-                break t1;
-            }
+                      if (option == 2) {
+                          break t1;
+                      }
+                  }
+              }
+              else {
+                  System.out.println("Please enter the train number as mentioned");
+                  break;
+              }
+          }
 
         }
     }
 
 
 
-}
+
+
